b00e24409bde509b013d8a3c3b1816ed5343ceee
JackyFTW <jackmbarter@gmail.com>
2023-09-23 14:09:43 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Button from '@mui/joy/Button';
+
+function App() {
+  return (
+    <div>
+      <Button variant="solid">Give</Button>
+      <Button variant="soft">Your</Button>
+      <Button variant="outlined">Balls</Button>
+      <Button variant="plain">a Tug</Button>
+    </div>
+  );
+}
+
+export default App;
